7c83c1e64a6db0b4a58b640c2a6953b53a1b95ab
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-08-12 22:00:08 +0530

    serverconfig updated

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,5 +1,5 @@
 const dotenv=require('dotenv')
-const { FRONTEND_URL } = require('../../../FoodComa-Backend/src/config/serverConfig')
+
 
 dotenv.config()
 
@@ -7,6 +7,5 @@ module.exports={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY,
-    FRONTEND_URL:process.env.FRONTEND_URL
+    JWT_EXPIRY: process.env.JWT_EXPIRY
 }

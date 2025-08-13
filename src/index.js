994e7dbf6c66a91e8a72e2114998f3234d3cb6ee
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-08-13 11:00:43 +0530

    update

--- src/index.js
+++ src/index.js
@@ -13,11 +13,20 @@ const cors = require('cors');
 
 const allowedOrigins = [
     'http://localhost:5173',
-    process.env.FRONTEND_URL 
+    process.env.FRONTEND_URL  
 ];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function(origin, callback) {
+       
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 

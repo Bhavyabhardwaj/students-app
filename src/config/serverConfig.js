37f8304a959693a0aed95b2eaece3aac93f85fe6
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-08-13 11:08:37 +0530

    frontend url added in config file

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -7,5 +7,6 @@ module.exports={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    FRONTEND_URL:process.env.FRONTEND_URL
 }

6c61f37e3d082feb722d54bad5d051348b2c7c47
Ashish0479 <vp.ashish.2016@gmail.com>
2025-08-22 12:45:14 +0530

    Update authValidator.js

--- src/validations/authValidator.js
+++ src/validations/authValidator.js
@@ -6,6 +6,9 @@ async function isLoggedIn(req, res, next) {
     
     const token = req.cookies["authToken"];
     console.log(token);
+     if(req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        token = req.headers.authorization.split(" ")[1];
+    }
     if(!token) {
         return res.status(401).json({
             success: false,
